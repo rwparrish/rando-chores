786959cc907a943ceba4ebf0e9865058d71972a7
rwparrish <rino3717@gmail.com>
2020-12-08 12:17:03 -0700

    Update random_chores.rb

--- random_chores.rb
+++ random_chores.rb
@@ -9,18 +9,14 @@ chores = [ "vacuum upstairs",
 
 users = [ "Tiny", "Monkey" ]
 
-def pick_user(users, input)
-  winner = users.sample
-end
-
-def verify_input
-  if verify_input
-end
-
-
-def assign_chores(winner, list_of_chores)
+def assign_chores(users, input, chores)
+  
 
 end
+
+
+
+
 
 puts "CHORE ASSIGNER has been activated. Hello and Zoë, hello Adette."
 25.times { print "-" }
@@ -32,13 +28,13 @@ puts "Creating a list of chores..."
 # sleep(7)
 puts "Get ready to know which chores you will be doing!"
 # sleep(7)
-puts "First, lets see who will go first!"
-# sleep(4)
+
+attempts = 1
+while attempts < 4
 puts "Please enter 'chores' on the keyboard."
-input = gets.chomp.strip
-verify_input(input)
-kid = pick_user(users, input)
-puts kid
+chores = gets.chomp.strip
+attempts += 1
+end
 
   
   
